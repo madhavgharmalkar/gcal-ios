975f409b335cbf9028790d45f5a9b3f1e01e6929
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2022-10-17 14:14:10 -0700

    upgrades iOS version + adds first swift file

---
+++ Classes/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Gaudiya Calendar
+//
+//  Created by Madhav Gharmalkar on 10/17/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
