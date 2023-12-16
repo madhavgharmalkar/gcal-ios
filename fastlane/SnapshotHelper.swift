98e4fe46c83eb82ea62ebf1f377fb38d965b47ed
Madhav Gharmalkar <gharmalkar.madhav@gmail.com>
2023-12-15 16:48:44 -0800

    updates to the uploader

--- fastlane/SnapshotHelper.swift
+++ fastlane/SnapshotHelper.swift
@@ -180,7 +180,7 @@ open class Snapshot: NSObject {
 
                 let path = screenshotsDir.appendingPathComponent("\(simulator)-\(name).png")
                 #if swift(<5.0)
-                    try UIImagePNGRepresentation(image)?.write(to: path, options: .atomic)
+                    UIImagePNGRepresentation(image)?.write(to: path, options: .atomic)
                 #else
                     try image.pngData()?.write(to: path, options: .atomic)
                 #endif
@@ -305,4 +305,4 @@ private extension CGFloat {
 
 // Please don't remove the lines below
 // They are used to detect outdated configuration files
-// SnapshotHelperVersion [1.29]
+// SnapshotHelperVersion [1.28]
